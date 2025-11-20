3f6d5fb4fe87fba81b505d860564281592dd8abe
KDS <qidhebd@gmail.com>
2025-11-20 11:20:19 +0900

    first commit

---
+++ src/main/java/com/example/Dream/User.java
@@ -0,0 +1,62 @@
+package com.example.Dream;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class User implements Serializable {
+
+    @Id
+    @Column(name="email")
+    private String email;
+
+    @Column(name="password", nullable = false)
+    private String password;
+
+    @Column(name="name", nullable = false)
+    private String name;
+
+    @Column(name="join_date")
+    @CreatedDate
+    private LocalDateTime joinDate;
+
+    @ManyToOne
+    private User writer;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(LocalDateTime joinDate) {
+        this.joinDate = joinDate;
+    }
+}
